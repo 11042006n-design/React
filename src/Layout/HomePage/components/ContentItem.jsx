db7ce4731c539d327caec3035ac0a470636568f5
Nastya <11042006n@gmail.com>
2026-08-13 21:48:39 +0300

    frontend

--- src/Layout/HomePage/components/ContentItem.jsx
+++ src/Layout/HomePage/components/ContentItem.jsx
@@ -1,9 +1,7 @@
-const ContentItem=()=>{
+const ContentItem=(props)=>{
     return (
   <div >
-
-    <div className="row">
-      {Array.from({ length: 5 }, (_, index) => (
+      {props.setOfLabels.map((item,index) => (
         <div className="card m-3" key={index} style={{ width: '18rem', margin: '10px' }}>
           <img 
             src={`https://loremflickr.com/${200 + index}/200`}
@@ -11,16 +9,12 @@ const ContentItem=()=>{
             alt="Card image" 
           />
           <div className="card-body">
-            <h5 className="card-title">Card title {index + 1}</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <button className="btn btn-primary">Go somewhere</button>
+            <h5 className="card-title">{item.cardTitle}</h5>
+            <p className="card-text">{item.cardText}</p>
           </div>
         </div>
       ))}
     </div>
-  </div>
     );
 }
 
